cd13f46b6c28c887f8b49ae373d6749dbeded1dc
JonErikDSuero <jonerikdsuero@gmail.com>
2015-11-22 17:34:45 -0500

    implemented "klass.nil_twin="

--- lib/nil_twin.rb
+++ lib/nil_twin.rb
@@ -4,30 +4,20 @@ module NilTwin
 
   def self.included(klass)
     puts "included NilTwin #{VERSION}!"
-
-
     nil_twin_name = "#{klass}NilTwin"
 
-    ClassMethods.nil_twin = nil_twin_name.safe_constantize || Object.const_set(nil_twin_name, Class.new(klass))
-
+    klass.class_attribute(:nil_twin)
+    klass.nil_twin = nil_twin_name.safe_constantize || Object.const_set(nil_twin_name, Class.new(klass))
     klass.extend(ClassMethods)
   end
 
+
   module ClassMethods # (self.)
-
-    def nil_twin
-      @@nil_twin
-    end
-
-    def self.nil_twin=(klass)
-      @@nil_twin = klass
-    end
-
     def find(*args)
       begin
         super
       rescue ActiveRecord::RecordNotFound
-        @@nil_twin.new
+        self.nil_twin.new
       end
     end
   end
