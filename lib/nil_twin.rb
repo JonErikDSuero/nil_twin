6d35160a08900af278d0449fff1abd0fdf07a6df
JonErikDSuero <jonerikdsuero@gmail.com>
2015-11-22 16:50:19 -0500

    creates niltwin

--- lib/nil_twin.rb
+++ lib/nil_twin.rb
@@ -1,5 +1,35 @@
 require "nil_twin/version"
 
 module NilTwin
-  # Your code goes here...
+
+  def self.included(klass)
+    puts "included NilTwin #{VERSION}!"
+
+
+    nil_twin_name = "#{klass}NilTwin"
+
+    ClassMethods.nil_twin = nil_twin_name.safe_constantize || Object.const_set(nil_twin_name, Class.new(klass))
+
+    klass.extend(ClassMethods)
+  end
+
+  module ClassMethods # (self.)
+
+    def nil_twin
+      @@nil_twin
+    end
+
+    def self.nil_twin=(klass)
+      @@nil_twin = klass
+    end
+
+    def find(*args)
+      begin
+        super
+      rescue ActiveRecord::RecordNotFound
+        @@nil_twin.new
+      end
+    end
+  end
+
 end
