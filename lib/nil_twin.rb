bdaed5f680e9a7f0749cb0230efb9f9c231d8e86
JonErikDSuero <jonerikdsuero@gmail.com>
2015-11-22 18:32:16 -0500

    add "like nil instance methods"

--- lib/nil_twin.rb
+++ lib/nil_twin.rb
@@ -6,8 +6,11 @@ module NilTwin
     puts "included NilTwin #{VERSION}!"
     nil_twin_name = "#{klass}NilTwin"
 
+    nil_twin = nil_twin_name.safe_constantize || Object.const_set(nil_twin_name, Class.new(klass))
+    nil_twin.include(LikeNilInstanceMethods)
+
     klass.class_attribute(:nil_twin)
-    klass.nil_twin = nil_twin_name.safe_constantize || Object.const_set(nil_twin_name, Class.new(klass))
+    klass.nil_twin = nil_twin
     klass.extend(ClassMethods)
   end
 
@@ -15,6 +18,21 @@ module NilTwin
   module ClassMethods # (self.)
     def find_by(*args)
       super || self.nil_twin.new
+    end
+  end
+
+
+  module LikeNilInstanceMethods
+    def nil?
+      true
+    end
+
+    def blank?
+      true
+    end
+
+    def present?
+      false
     end
   end
 
